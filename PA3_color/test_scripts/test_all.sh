30d9cd918a516cce18faec62f1fcd1846ef66720
mike84265 <b03901078@ntu.edu.tw>
2017-05-02 13:39:23 +0800

    PA3 ver 1.1

--- PA3_color/test_scripts/test_all.sh
+++ PA3_color/test_scripts/test_all.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
-bin/graphlab -f inputs/mycase/gn4.dot
-ref/graphlab_ref -f inputs/ref/gn4.dot
-bin/graphlab -f inputs/mycase/gn8.dot
-ref/graphlab_ref -f inputs/ref/gn8.dot
-bin/graphlab -f inputs/mycase/gn10.dot
-ref/graphlab_ref -f inputs/ref/gn10.dot
-bin/graphlab -f inputs/mycase/gn100.dot
-ref/graphlab_ref -f inputs/ref/gn100.dot
-bin/graphlab -f inputs/mycase/gn1000.dot
-ref/graphlab_ref -f inputs/ref/gn1000.dot
-bin/graphlab -f inputs/mycase/gn10000.dot
-ref/graphlab_ref -f inputs/ref/gn10000.dot
+v[0]='4'
+v[1]='8'
+v[2]='10'
+v[3]='100'
+v[4]='1000'
+v[5]='10000'
+
+rm mydata
+rm refdata
+for i in $(seq 0 5); do
+echo "gn${v[$i]}.bat"
+bin/graphlab -f inputs/mycase/gn${v[$i]}.bat >> mydata
+ref/graphlab_ref -f inputs/ref/gn${v[$i]}.bat >> refdata
+done
+
+for i in $(seq 0 5); do
+echo "comparing gn4..."
+diff outputs/mycase/gn${v[$i]}_dfs.dot outputs/ref/gn${v[$i]}_dfs.dot 
+diff outputs/mycase/gn${v[$i]}_bfs.dot outputs/ref/gn${v[$i]}_bfs.dot 
+diff outputs/mycase/gn${v[$i]}_color.dot outputs/ref/gn${v[$i]}_color.dot 
+done
